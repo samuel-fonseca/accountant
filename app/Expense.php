aef79ab6a2f0bbebffe22bb1c34fcb6ce7ddd18e
Samuel Fonseca <samuel.fonseca96@gmail.com>
2020-06-08 17:30:15 -0400

    Defined relationships

--- app/Expense.php
+++ app/Expense.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Expense extends Model
 {
     use UuidPrimaryKey;
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function account()
+    {
+        return $this->belongsTo('App\Account');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
