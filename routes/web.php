a33dd73f0c33b5d40b69f6b16097db64e0161e72
Samuel Fonseca <samuel.fonseca96@gmail.com>
2020-06-08 12:54:41 -0400

    Removed lines

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 // Route::resource('/dashboard', 'DashboardsController');
 
+// Global routing - Routing is handled by Vue-Router
 Route::get('{any?}', function () {
     return view('app');
 })->where('any', '.*');
