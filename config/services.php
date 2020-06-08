0c0942eeb33231c24e0f4d583073eaf01cefb57f
Samuel Fonseca <samuel.fonseca96@gmail.com>
2020-06-08 17:29:43 -0400

    Updated service config Removed `url()` function as it caused errors

--- config/services.php
+++ config/services.php
@@ -31,7 +31,7 @@ return [
     ],
 
     'passport' => [
-        'login_endpoint' => env('PASSPORT_LOGIN_ENDPOINT', url('/oauth/token')),
+        'login_endpoint' => env('PASSPORT_LOGIN_ENDPOINT'),
         'client_id' => env('PASSPORT_CLIENT_ID'),
         'client_secret' => env('PASSPORT_CLIENT_SECRET'),
     ],
